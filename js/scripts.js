0eccb1d25e1abeb501847e42aef3877368304028
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-29 11:01:51 -0800

    target output spans in scripts to display values

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@
 function Pizza(sauce, cheese, meat, veggie, extra, size){
   this.sauce = sauce;
   this.cheese = cheese;
-  this.meatTopping = meat;
-  this.vegtopping = veggie;
-  this.extraTopping = extra;
+  this.meat = meat;
+  this.veggie = veggie;
+  this.extra = extra;
   this.size = size;
 }
 
 Pizza.prototype.price = function(){
   this.price = 0;
-  if(this.size === 'small'){
+  if(this.size === 'Small'){
     this.price = '$5';
-  } else if (this.size === 'medium'){
+  } else if (this.size === 'Medium'){
     this.price = '$8';
-  } else if (this.size === 'large'){
+  } else if (this.size === 'Large'){
     this.price = '$12';
   } else {
     this.price = 0;
@@ -42,7 +42,7 @@ $(document).ready(function(){
     const veggie = $('select#veggies').val();
     const extras =$('select#extras').val();
     let newPizza = new Pizza(sauce, cheese, meats, veggie, extras, size);
-    
+    console.log(newPizza);
     if(name === ''){
       $('div#warning').text("Please enter your name");
     } else if(size === ''){
@@ -59,10 +59,22 @@ $(document).ready(function(){
       $('div#warning').text("Please enter any extras");
     } else {
       $('div#warning').empty();
+      $('form').hide();
+      $('div#output').show();
     }
 
-
-
+    const userName = newPizza.names();
+    console.log(userName);
+    const price = newPizza.price();
+    console.log(price);
+    $('span#user-name').text(newPizza.name);
+    $('span#user-size').text(newPizza.size);
+    $('span#user-sauce').text(newPizza.sauce);
+    $('span#user-cheese').text(newPizza.cheese);
+    $('span#user-meat').text(newPizza.meat);
+    $('span#user-veggie').text(newPizza.veggie);
+    $('span#user-extra').text(newPizza.extra);
+    $('span#user-price').text(newPizza.price);
   });
 
 
